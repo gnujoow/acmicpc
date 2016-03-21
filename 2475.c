fc1b6bf2a5817eb09d0d5c3226aed7c252ba6c51
osx_gnujoow <gnujoow.kim@gmail.com>
2016-03-21 16:24:20 +0900

    2475

---
+++ 2475.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int num[5];
+	int check = 0;
+	for (int i = 0 ; i < 5 ; i++)
+	{
+		scanf("%d",&num[i]);
+		check += num[i] * num[i];
+	}
+
+	printf("%d", check%10);
+
+
+	return 0;
+}
